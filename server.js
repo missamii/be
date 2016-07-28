9f0ef45a8b00989c9a7edb288a60b16e61b099a1
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-07-27 23:49:45 -0400

    working on favorite button feature still share feature works.

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var app = express();
 var request = require('request');
-PORT = process.env.PORT || 80;
+PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
@@ -16,10 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // connect to the database server
 var MongoClient = mongodb.MongoClient;
 
-var GOOGLE_BOOKS_API = '';
+var GOOGLE_BOOKS_API = process.env.GOOGLE_BOOKS_API;
 
-var url = 'mongodb://heroku_jp0xr3w7:juearsuhnp8gppac1n7t31s4bq@ds029745.mlab.com:29745/heroku_jp0xr3w7/books_db';
-// var url = 'mongodb://localhost:27017/books_db';
+
+// var url = 'mongodb://heroku_jp0xr3w7:juearsuhnp8gppac1n7t31s4bq@ds029745.mlab.com:29745/heroku_jp0xr3w7/books_db';
+
+var url = 'mongodb://localhost:27017/books_db';
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, database) {
   if (err) {
@@ -28,12 +29,14 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, datab
   }
 });
 
+// THANK GOD for unicorns demo & my marvel hw for the skelton of the code below.
+
 // testing
 app.get("/", function(req, res){
   res.json({"description":"HI TIFFANY"});
 });
 
-
+//get all books
 app.get("/books", function(req, res){
   MongoClient.connect(mongoUrl, function (err, db) {
      var favbooksCollection = db.collection('books');
@@ -50,7 +53,7 @@ app.get("/books", function(req, res){
            response.json(result);
          } else { //
            console.log('No document(s) found with defined "find" criteria');
-           response.json("no unicorns found");
+           response.json("nothing found");
          }
          db.close(function() {
            console.log( "database CLOSED");
@@ -71,18 +74,16 @@ app.post("/books/favorites", function(req, res){
      } else {
        // We are connected!
        console.log('Connection established to', mongoUrl);
-       console.log('Adding new user...');
+       console.log('add new book!');
 
-       /* Insert */
+       // Insert new favorite book!
        var newFavoriteBook = request.body;
-       favbooksCollection.insert([newFavoriteBook], function (err, result) {
+       favbooksCollection.insert(data, function (err, result) {
          if (err) {
            console.log(err);
            response.json("error");
          } else {
-           console.log('Inserted.');
            console.log('RESULT!!!!', result);
-           console.log("end result");
            response.json(result);
          }
          db.close(function() {
@@ -93,7 +94,71 @@ app.post("/books/favorites", function(req, res){
    }); // end mongo connect
  }); // end add new
 
-app.post("/books", function(req, res){
+ // find book
+ app.get('/books/:name', function(req, res){
+   // response.json({"description":"find by name"});
+   // console.log("request.params: ", request.params);
+   MongoClient.connect(mongoUrl, function (err, db) {
+     var favbooksCollection = db.collection('books');
+     if (err) {
+       console.log('Unable to connect to the mongoDB server. ERROR:', err);
+     } else {
+       // We are connected!
+       console.log('Finding your saved book... ');
+       /* Get */
+       favebooksCollection.find(request.params).toArray(function (err, result) {
+         if (err) {
+           console.log("ERROR!", err);
+           res.json("error");
+         } else if (result.length) {
+           console.log('Found:', result);
+           res.json(result);
+         } else { //
+           console.log('No document(s) found with defined "find" criteria');
+           res.json("no books found");
+         }
+         db.close(function() {
+           console.log( "database CLOSED");
+         });
+       }); // end find
+     } // end else
+   }); // end mongo connect
+
+ });
+
+// delete favorite book!
+app.delete('/favorites/:_id', function(req, res) {
+ // console.log("request.body:", request.body);
+ console.log("request.params:", request.params._id);
+ // console.log(request.body.id);
+ MongoClient.connect(mongoUrl, function (err, db) {
+   var favbooksCollection = db.collection('books');
+   if (err) {
+     console.log('Unable to connect to the mongoDB server. ERROR:', err);
+   } else {
+     console.log('Connection established to', mongoUrl);
+     console.log('Goodbye book...');
+
+     favbooksCollection.remove({_id: ObjectId(request.body.id)}, function(err, result) {
+     if (err) {
+           console.log("ERROR!", err);
+           res.json("error");
+         } else if (result.length) {
+           console.log('Found:', result);
+           res.json(result);
+         } else {
+           console.log('No document(s) found with defined "find" criteria');
+           res.json("none found");
+         }
+         db.close(function() {
+           console.log( "database CLOSED");
+         });
+       });//end remove
+     } // end else
+   }); // end mongo connect
+});//Delete Closing Tags
+
+app.post("/", function(req, res){
 
 // stuff will go here
 
@@ -108,7 +173,6 @@ res.send(body);
         }
     });
 });//end post
-
 
 app.listen(PORT, function(){
   console.log('listening...');
